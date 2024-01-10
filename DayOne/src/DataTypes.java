eeb0114b8467563579fe7fb36df410b250efb47b
Rushikesh <Rushikesh@DESKTOP-5RFJV5I>
2024-01-10 22:36:30 +0530

    Day one programs

---
+++ DayOne/src/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		int val1 = 10 / 5; 
+		float val2 = 10f / 5f;
+		double val3 =10d / 5d;
+		float val4 = 5/2.5f; 
+		System.out.println("val 1=" + val1); 
+		System.out.println("val 2=" + val2);
+		System.out.println("val 3=" + val3);
+		System.out.println("val 4=" + val4);
+		
+		int marker = 512;
+		double percentage = marker * 0.46f;
+		System.out.println("percentage :" + percentage);
+
+	}
+}
---
+++ DayOne/src/Identifires.java
@@ -0,0 +1,14 @@
+public class Identifires {
+
+	public static void main(String[] args) {
+		int number=15;
+		System.out.println("value : "+ number);
+		int $number = 20; 
+		System.out.println("value : "+ $number);
+
+		String studName="Rutuja"; 
+		System.out.println("value : "+ studName);
+
+	}
+
+}
