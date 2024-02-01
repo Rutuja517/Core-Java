703c9fbf8cb374524ae907eeb0279172ae45bc2b
Rushikesh <Rushikesh@DESKTOP-5RFJV5I>
2024-02-01 20:15:59 +0530

    Method Overloading

---
+++ DaySeven/src/com/tnsif/overloading/MethodOverloading.java
@@ -0,0 +1,21 @@
+package com.tnsif.overloading;
+
+public class MethodOverloading {
+	public static int addition(int a,int b)
+	{
+		return a+b;
+	}
+	public static double addition(double a,int b)
+	{
+		return a+b;
+	}
+	public static float addition(float a,float b)
+	{
+		return a+b;
+	}
+	public static String addition(String a,String b)
+	{
+		return a+b;
+	}
+
+}
---
+++ DaySeven/src/com/tnsif/overloading/OverloadingDriver.java
@@ -0,0 +1,13 @@
+package com.tnsif.overloading;
+
+public class OverloadingDriver {
+
+	public static void main(String[] args) {
+		System.out.println(MethodOverloading.addition(10,20));
+		System.out.println(MethodOverloading.addition(10.6,20));
+		System.out.println(MethodOverloading.addition(1.6f, 0.8f));
+		System.out.println(MethodOverloading.addition("hii","hello"));
+
+	}
+
+}
