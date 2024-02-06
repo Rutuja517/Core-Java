4060bf807737405d110a2b50142307d6498b9942
Rushikesh <Rushikesh@DESKTOP-5RFJV5I>
2024-02-06 18:51:01 +0530

    Encapsulation

---
+++ Day3/src/com/tnsif/encap/EncapDemo.java
@@ -0,0 +1,46 @@
+package com.tnsif.encap;
+
+public class EncapDemo {
+	private int serialNum;
+	private String name;
+	private int age;
+
+	//Getters and Setters
+	public int getSerialNum() {
+		return serialNum;
+	}
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	//DefaultConstructor
+
+	public EncapDemo() {
+		super();
+	}
+	//constructor
+	public EncapDemo(int serialNum, String name, int age) {
+		super();
+		this.serialNum = serialNum;
+		this.name = name;
+		this.age = age;
+	}
+
+	//Object Class method
+	@Override
+	public String toString() {
+		return "EncapDemo [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
+	}
+}
---
+++ Day3/src/com/tnsif/encap/EncapMain.java
@@ -0,0 +1,12 @@
+package com.tnsif.encap;
+
+public class EncapMain {
+	public static void main(String[] args) {
+		EncapDemo a = new EncapDemo();
+		a.setSerialNum(1);
+		a.setName("Rutuja");
+		a.setAge(23);
+
+		System.out.println(a);
+}
+}
