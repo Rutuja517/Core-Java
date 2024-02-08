982370dc6f6d773dcc51867435b11bd5b506af87
Rushikesh <Rushikesh@DESKTOP-5RFJV5I>
2024-02-08 18:41:02 +0530

    Constructor and Scanner

---
+++ Day4/src/com/tnsif/constructor/customerMain.java
@@ -0,0 +1,16 @@
+package com.tnsif.constructor;
+
+public class customerMain {
+	public static void main(String[] args) {
+		/*
+		 * constructorDemo obj = new constructorDemo(); obj.setCustomerName("Rutuja");
+		 * obj.setCustomerId(3); obj.setCustomerCity("Nashik");
+		 * //System.out.println("Customer ID: "+customerID+"\nCustomer Name: "
+		 * +customerName+"\nCustomer City: "+customerCity); System.out.println(obj);
+		 */
+
+		constructorDemo obj1 = new constructorDemo("Rutu",5,"Nashik");
+		System.out.println(obj1);
+
+	}
+}
---
+++ Day4/src/com/tnsif/scanner/ScannerDemo.java
@@ -0,0 +1,36 @@
+package com.tnsif.scanner;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+	
+	public static void main(String[] args) {
+		System.out.println("Enter number:");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		if(num ==1) {
+			System.out.println("One");
+		}
+		else {
+			if(num ==2) {
+				System.out.println("Two");
+			}
+			else {
+				if(num ==3) {
+					System.out.println("Three");
+				}
+				else {
+					System.out.println("Other");
+				}
+
+			}
+
+		}
+		sc.close();
+
+	}
+
+}
+
+
+
